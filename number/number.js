16a35230e85df2cb7349bd4610cca7f33796e01e
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 18:02:14 +0700

    number

---
+++ number/number.js
@@ -0,0 +1,10 @@
+console.log("hello world".toUpperCase());
+let a = 5;
+let b = 6;
+let c = "5";
+let d = +c; //convert string to number
+let h = Number(c); //convert string to number
+console.log(a + c);
+console.log(a + b);
+console.log(a - c);
+console.log(a === c, "kieu a: ", typeof a, "kieu c: ", typeof c);
