72993349b70bc906df8e0fd87f6e14512a86c33b
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 20:37:06 +0700

    Arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,21 @@
+let b = ["MINH", "MAI", "XU", "ONG"];
+console.log("TYPE of b: ", typeof b, b);
+console.log("second item", b[1], "third item", b[2]);
+let c = [
+  {
+    name: "Amie",
+    age: "50",
+    city: "London",
+  },
+  {
+    name: "Hammer",
+    age: "40",
+    city: "Hills",
+  },
+  {
+    name: "latable",
+    age: "47",
+    city: "HCM",
+  },
+];
+console.log("NAME IN SECOND OBJ", c[1].age);
