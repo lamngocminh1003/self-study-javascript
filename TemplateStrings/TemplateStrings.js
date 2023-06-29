0360a6201ef3ebaa4173f7da21c317b6191e4762
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 19:01:01 +0700

    TemplateStrings

---
+++ TemplateStrings/TemplateStrings.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+let a = 5;
+let b = 10;
+console.log(`fifteen is ${a + b} and
+twenty is ${a * 2 + b}`);
+const url = "https://github.com/lamngocminh1003/self-study-javascript";
+console.log(`duong link la ${url}`);
