bddbb6e91b8d82053f08eecaa26b385d48bfe29d
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 15:23:59 +0700

    variables,constants,comment

---
+++ variables,constants,comment/variables,constants,comment.js
@@ -0,0 +1,12 @@
+let a = 6;
+let b = 4.99;
+let y = 9,
+  z = 7,
+  e = 9;
+let c = "Amie"; // thay doi dc gia tri
+const x = 6; //khong bh thay doi gia tri
+console.log(a == b);
+console.log("kieu cua c la: ", typeof c);
+console.log("kieu cua a la: ", typeof a);
+console.log("kieu cua b la: ", typeof b);
+console.log("Test number: ", "x= ", x, "y= ", y, "z= ", z);
