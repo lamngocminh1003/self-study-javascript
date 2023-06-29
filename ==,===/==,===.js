801026cd2dec4ed6855b82ccc015a64bd90b71dc
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 21:42:05 +0700

    Arrays

---
+++ ==,===/==,===.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = "5";
+console.log("compare a vs b: ", `a=${a}`, `b=${b}`, "result: ", b === a);
+//dung === chu kh xai == de chac chan moi thu chuan de so sanh gia tri
+//empty khi khai bao bien hay chuoi khong co gia tri
