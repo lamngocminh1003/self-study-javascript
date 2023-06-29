75f2c3133a0e2cf1eb94203ee99f8b5924d797a2
Amie <0950080034@sv.hcmunre.edu.vn>
2023-06-29 17:03:45 +0700

    Strings

---
+++ Strings/Strings.js
@@ -0,0 +1,7 @@
+let a = "chuoi abcd    ";
+console.log(a);
+console.log(4 + 5 + a); //vi cong tu trai sang phai
+console.log(a + 4 + 5);
+console.log(a.length); //so luong ky tu trong chuoi
+console.log(a.trim().length); //bo khoang trang truoc sau
+console.log(`hello "Amie"`);
