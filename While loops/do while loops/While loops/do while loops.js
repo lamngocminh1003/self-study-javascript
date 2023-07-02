25cb1abba5c687da46bf649a26ecf1022a315e34
Amie <0950080034@sv.hcmunre.edu.vn>
2023-07-02 21:52:19 +0700

    While loops/do while loops

---
+++ While loops/do while loops/While loops/do while loops.js
@@ -0,0 +1,10 @@
+console.log("hello world hihi");
+let arrTop4 = ["MU", "LIVER", "MU", "Chelsea"];
+// for (let i = 0; i < arrTop4.length; i++) {
+//   console.log("Top", i + 1, arrTop4[i]);
+// }
+var i = 0;
+while (i < arrTop4.length) {
+  console.log("Top", i + 1, arrTop4[i]);
+  i += 1;
+}
