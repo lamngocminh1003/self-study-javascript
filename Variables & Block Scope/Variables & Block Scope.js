d182d7eec247c42d91bacd5e8112a6fd5e3188d1
Amie <0950080034@sv.hcmunre.edu.vn>
2023-07-03 20:51:26 +0700

    Declare function

---
+++ Variables & Block Scope/Variables & Block Scope.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+for (let i = 0; i < 10; i++) {
+  console.log("check log:", i);
+}
+function sum(a, b) {
+  return a + b;
+}
+let c = sum(1, 2);
+console.log("sum a+b=", c);
