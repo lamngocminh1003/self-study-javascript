c35d9bf2a0da9d9e6fb35221f31c6a76f18b1150
Amie <0950080034@sv.hcmunre.edu.vn>
2023-07-03 22:06:47 +0700

    Arrow function

---
+++ Returning values/Returning values.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+function getName(lastName, firstName) {
+  return lastName + " " + firstName;
+}
+c = getName("Minh", "Lam");
+console.log("My name is " + c);
+
+sum = (a, b) => {
+  return a * b;
+};
+d = sum(5, 5);
+console.log("check d: " + d);
